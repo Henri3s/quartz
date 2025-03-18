055c3897d559958a79f7f5864f1c48e81e2a3b69
Henri <henri.zhou@icloud.com>
2025-03-18 16:22:08 +0800

    Quartz sync: Mar 18, 2025, 4:22 PM

--- quartz/plugins/emitters/contentIndex.tsx
+++ quartz/plugins/emitters/contentIndex.tsx
@@ -87,7 +87,7 @@ function generateRSSFeed(cfg: GlobalConfiguration, idx: ContentIndexMap, limit?:
       <description>${!!limit ? i18n(cfg.locale).pages.rss.lastFewNotes({ count: limit }) : i18n(cfg.locale).pages.rss.recentNotes} on ${escapeHTML(
         cfg.pageTitle,
       )}</description>
-      <generator>Quartz -- quartz.jzhao.xyz</generator>
+      <generator>圳.中国--Henri.Z</generator>
       ${items}
     </channel>
   </rss>`
